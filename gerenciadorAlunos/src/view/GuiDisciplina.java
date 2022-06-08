2e9e0347b885f6b6ef2ed5f679164185a4271caf
SB-6563 <61550355+SB-6563@users.noreply.github.com>
2022-06-07 22:03:05 -0300

    v0.7.6
    Correção da forma que foi criado as telas
    - De Window para Frame - Turma e Disciplina

--- gerenciadorAlunos/src/view/GuiDisciplina.java
+++ gerenciadorAlunos/src/view/GuiDisciplina.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -9,20 +11,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 import model.Disciplina;
 import model.DisciplinaDAO;
 import model.TabelaDisciplina;
-import javax.swing.JScrollPane;
-import java.awt.Font;
 
 @SuppressWarnings("serial")
-public class GuiDisciplina extends JFrame{
+public class GuiDisciplina extends JFrame {
 
-	private JFrame frmDisciplina;
+	private JPanel contentPane;
 	private JTextField txtCodigo;
 	private JTextField txtNome;
 	private JTextField txtArea;
@@ -36,8 +38,8 @@ public class GuiDisciplina extends JFrame{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GuiDisciplina window = new GuiDisciplina();
-					window.frmDisciplina.setVisible(true);
+					GuiDisciplina frame = new GuiDisciplina();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -46,64 +48,67 @@ public class GuiDisciplina extends JFrame{
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public GuiDisciplina() {
+		
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1264, 681);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(471, 0, 773, 681);
+		panel.add(scrollPane);
+
+		tbDisciplinas = new JTable();
 		this.modeloTabela = new TabelaDisciplina();
 		this.tbDisciplinas = new JTable(modeloTabela);
-		tbDisciplinas.setFont(new Font("Arial", Font.PLAIN, 20));
-		initialize();
-	}
+		tbDisciplinas.setFont(new Font("Arial", Font.PLAIN, 14));
+		scrollPane.setViewportView(tbDisciplinas);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmDisciplina = new JFrame();
-		frmDisciplina.setTitle("Disciplina");
-		frmDisciplina.setBounds(100, 100, 1280, 720);
-		frmDisciplina.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmDisciplina.getContentPane().setLayout(null);
-		
-		txtCodigo = new JTextField();
-		txtCodigo.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtCodigo.setBounds(126, 26, 150, 20);
-		frmDisciplina.getContentPane().add(txtCodigo);
-		txtCodigo.setColumns(10);
-		
+		JLabel lblId = new JLabel("C\u00F3digo");
+		lblId.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblId.setBounds(10, 11, 65, 37);
+		panel.add(lblId);
+
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNome.setBounds(10, 78, 65, 37);
+		panel.add(lblNome);
+
+		JLabel lblEndereco = new JLabel("Per\u00EDodo");
+		lblEndereco.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblEndereco.setBounds(10, 156, 72, 37);
+		panel.add(lblEndereco);
+
 		txtNome = new JTextField();
 		txtNome.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtNome.setBounds(126, 78, 477, 36);
-		frmDisciplina.getContentPane().add(txtNome);
+		txtNome.setBounds(83, 81, 217, 31);
+		panel.add(txtNome);
 		txtNome.setColumns(10);
-		
+
 		txtArea = new JTextField();
 		txtArea.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtArea.setBounds(126, 150, 477, 36);
-		frmDisciplina.getContentPane().add(txtArea);
 		txtArea.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("C\u00F3digo");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 28, 150, 20);
-		frmDisciplina.getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Nome");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(10, 86, 150, 20);
-		frmDisciplina.getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("\u00C1rea");
-		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(34, 161, 92, 14);
-		frmDisciplina.getContentPane().add(lblNewLabel_2);
-		
-		JButton btnInserir = new JButton("Inserir");
-		btnInserir.setFont(new Font("Arial", Font.PLAIN, 26));
-		btnInserir.addActionListener(new ActionListener() {
+		txtArea.setBounds(83, 159, 217, 31);
+		panel.add(txtArea);
+
+		txtCodigo = new JTextField();
+		txtCodigo.setFont(new Font("Arial", Font.PLAIN, 20));
+		txtCodigo.setBounds(85, 14, 103, 31);
+		panel.add(txtCodigo);
+		txtCodigo.setColumns(10);
+
+		JButton btnIncluir = new JButton("Incluir");
+		btnIncluir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Disciplina d = new Disciplina();
 				DisciplinaDAO dDAO = new DisciplinaDAO();
@@ -113,72 +118,72 @@ public class GuiDisciplina extends JFrame{
 				
 				dDAO.inserir(d);
 				
-				JOptionPane.showMessageDialog(btnInserir, "Disciplina inserida com sucesso");
+				JOptionPane.showMessageDialog(btnIncluir, "Disciplina inserida com sucesso");
 			}
 		});
-		btnInserir.setBounds(10, 260, 296, 205);
-		frmDisciplina.getContentPane().add(btnInserir);
-		
+		btnIncluir.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnIncluir.setBounds(10, 268, 225, 185);
+		panel.add(btnIncluir);
+
+		JButton btnAlterar = new JButton("Alterar");
+		btnAlterar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Disciplina d = new Disciplina();
+				DisciplinaDAO dDAO = new DisciplinaDAO();
+
+				d.setCod(Integer.parseInt(txtCodigo.getText()));
+				d.setNome(txtNome.getText());
+				d.setArea(txtArea.getText());
+
+				dDAO.alterar(d);
+
+				JOptionPane.showMessageDialog(btnIncluir, "Dados da Disciplina alterado com sucesso");
+			}
+		});
+		btnAlterar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnAlterar.setBounds(236, 268, 225, 185);
+		panel.add(btnAlterar);
+
 		JButton btnListar = new JButton("Listar");
-		btnListar.setFont(new Font("Arial", Font.PLAIN, 26));
 		btnListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				modeloTabela.cleanTabela();
-				
+
 				Disciplina d;
 				DisciplinaDAO dDAO = new DisciplinaDAO();
-				
+
 				ArrayList<Disciplina> lista = dDAO.listarTodos();
-				for(int i=0;i<lista.size();i++) {
+				for (int i = 0; i < lista.size(); i++) {
 					d = new Disciplina();
 					d.setCod(lista.get(i).getCod());
 					d.setNome(lista.get(i).getNome());
 					d.setArea(lista.get(i).getArea());
-					
+
 					modeloTabela.addDisciplina(d);
 				}
 				modeloTabela.fireTableDataChanged();
-				
+
 			}
 		});
-		btnListar.setBounds(10, 476, 296, 194);
-		frmDisciplina.getContentPane().add(btnListar);
-		
-		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setFont(new Font("Arial", Font.PLAIN, 26));
-		btnAlterar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Disciplina d = new Disciplina();
-				DisciplinaDAO dDAO = new DisciplinaDAO();
-				
-				d.setCod(Integer.parseInt(txtCodigo.getText()));
-				d.setNome(txtNome.getText());
-				d.setArea(txtArea.getText());
-				
-				dDAO.alterar(d);
-				JOptionPane.showMessageDialog(btnInserir, "Dados da disciplina alterado com sucesso");
-			}
-		});
-		btnAlterar.setBounds(316, 260, 287, 205);
-		frmDisciplina.getContentPane().add(btnAlterar);
-		
+		btnListar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnListar.setBounds(10, 453, 225, 217);
+		panel.add(btnListar);
+
 		JButton btnDeletar = new JButton("Deletar");
-		btnDeletar.setFont(new Font("Arial", Font.PLAIN, 26));
 		btnDeletar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DisciplinaDAO dDAO = new DisciplinaDAO();
-				
+
 				dDAO.delete(Integer.parseInt(txtCodigo.getText()));
-				JOptionPane.showMessageDialog(btnInserir, "Disciplina deletada com sucesso");
+
+				JOptionPane.showMessageDialog(btnIncluir, "Disciplina deletada com sucesso");
 			}
 		});
-		btnDeletar.setBounds(316, 476, 287, 194);
-		frmDisciplina.getContentPane().add(btnDeletar);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(613, 11, 641, 659);
-		frmDisciplina.getContentPane().add(scrollPane);
-		scrollPane.setViewportView(tbDisciplinas);
+		btnDeletar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnDeletar.setBounds(236, 453, 225, 217);
+		panel.add(btnDeletar);
 	}
+
 }
--- gerenciadorAlunos/src/view/GuiTurma.java
+++ gerenciadorAlunos/src/view/GuiTurma.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -9,20 +11,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
+import model.TabelaTurma;
 import model.Turma;
 import model.TurmaDAO;
-import model.TabelaTurma;
-import javax.swing.JScrollPane;
-import java.awt.Font;
 
 @SuppressWarnings("serial")
-public class GuiTurma extends JFrame{
+public class GuiTurma extends JFrame {
 
-	private JFrame frmTurma;
+	private JPanel contentPane;
 	private JTextField txtCodigo;
 	private JTextField txtSerie;
 	private JTextField txtPeriodo;
@@ -36,8 +38,8 @@ public class GuiTurma extends JFrame{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GuiTurma window = new GuiTurma();
-					window.frmTurma.setVisible(true);
+					GuiTurma frame = new GuiTurma();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -46,64 +48,66 @@ public class GuiTurma extends JFrame{
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public GuiTurma() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1264, 681);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(471, 0, 773, 681);
+		panel.add(scrollPane);
+
+		tbTurmas = new JTable();
 		this.modeloTabela = new TabelaTurma();
 		this.tbTurmas = new JTable(modeloTabela);
-		tbTurmas.setFont(new Font("Arial", Font.PLAIN, 20));
-		initialize();
-	}
+		tbTurmas.setFont(new Font("Arial", Font.PLAIN, 14));
+		scrollPane.setViewportView(tbTurmas);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmTurma = new JFrame();
-		frmTurma.setTitle("Turma");
-		frmTurma.setBounds(100, 100, 1280, 720);
-		frmTurma.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmTurma.getContentPane().setLayout(null);
-		
-		txtCodigo = new JTextField();
-		txtCodigo.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtCodigo.setBounds(126, 18, 150, 36);
-		frmTurma.getContentPane().add(txtCodigo);
-		txtCodigo.setColumns(10);
-		
+		JLabel lblId = new JLabel("C\u00F3digo");
+		lblId.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblId.setBounds(10, 11, 65, 37);
+		panel.add(lblId);
+
+		JLabel lblNome = new JLabel("Serie");
+		lblNome.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNome.setBounds(10, 78, 65, 37);
+		panel.add(lblNome);
+
+		JLabel lblEndereco = new JLabel("Per\u00EDodo");
+		lblEndereco.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblEndereco.setBounds(10, 156, 72, 37);
+		panel.add(lblEndereco);
+
 		txtSerie = new JTextField();
 		txtSerie.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtSerie.setBounds(126, 79, 477, 36);
-		frmTurma.getContentPane().add(txtSerie);
+		txtSerie.setBounds(83, 81, 217, 31);
+		panel.add(txtSerie);
 		txtSerie.setColumns(10);
-		
+
 		txtPeriodo = new JTextField();
 		txtPeriodo.setFont(new Font("Arial", Font.PLAIN, 20));
-		txtPeriodo.setBounds(126, 151, 477, 36);
-		frmTurma.getContentPane().add(txtPeriodo);
 		txtPeriodo.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("C\u00F3digo");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 24, 150, 25);
-		frmTurma.getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Serie");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setBounds(10, 87, 150, 20);
-		frmTurma.getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Periodo");
-		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(24, 157, 92, 25);
-		frmTurma.getContentPane().add(lblNewLabel_2);
-		
-		JButton btnInserir = new JButton("Inserir");
-		btnInserir.setFont(new Font("Arial", Font.PLAIN, 24));
-		btnInserir.addActionListener(new ActionListener() {
+		txtPeriodo.setBounds(83, 159, 217, 31);
+		panel.add(txtPeriodo);
+
+		txtCodigo = new JTextField();
+		txtCodigo.setFont(new Font("Arial", Font.PLAIN, 20));
+		txtCodigo.setBounds(85, 14, 103, 31);
+		panel.add(txtCodigo);
+		txtCodigo.setColumns(10);
+
+		JButton btnIncluir = new JButton("Incluir");
+		btnIncluir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Turma d = new Turma();
 				TurmaDAO dDAO = new TurmaDAO();
@@ -113,74 +117,72 @@ public class GuiTurma extends JFrame{
 				
 				dDAO.inserir(d);
 				
-				JOptionPane.showMessageDialog(btnInserir, "Turma inserida com sucesso");
+				JOptionPane.showMessageDialog(btnIncluir, "Turma inserida com sucesso");
 			}
 		});
-		btnInserir.setBounds(10, 260, 296, 205);
-		frmTurma.getContentPane().add(btnInserir);
-		
+		btnIncluir.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnIncluir.setBounds(10, 268, 225, 185);
+		panel.add(btnIncluir);
+
+		JButton btnAlterar = new JButton("Alterar");
+		btnAlterar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Turma d = new Turma();
+				TurmaDAO dDAO = new TurmaDAO();
+
+				d.setCod(Integer.parseInt(txtCodigo.getText()));
+				d.setSerie(txtSerie.getText());
+				d.setPeriodo(txtPeriodo.getText());
+
+				dDAO.alterar(d);
+
+				JOptionPane.showMessageDialog(btnIncluir, "Dados da turma alterado com sucesso");
+			}
+		});
+		btnAlterar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnAlterar.setBounds(236, 268, 225, 185);
+		panel.add(btnAlterar);
+
 		JButton btnListar = new JButton("Listar");
-		btnListar.setFont(new Font("Arial", Font.PLAIN, 24));
 		btnListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				modeloTabela.cleanTabela();
-				
+
 				Turma d;
 				TurmaDAO dDAO = new TurmaDAO();
-				
+
 				ArrayList<Turma> lista = dDAO.listarTodos();
-				for(int i=0;i<lista.size();i++) {
+				for (int i = 0; i < lista.size(); i++) {
 					d = new Turma();
 					d.setCod(lista.get(i).getCod());
 					d.setSerie(lista.get(i).getSerie());
 					d.setPeriodo(lista.get(i).getPeriodo());
-					
+
 					modeloTabela.addTurma(d);
 				}
 				modeloTabela.fireTableDataChanged();
-				
+
 			}
 		});
-		btnListar.setBounds(10, 476, 296, 194);
-		frmTurma.getContentPane().add(btnListar);
-		
-		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setFont(new Font("Arial", Font.PLAIN, 24));
-		btnAlterar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Turma d = new Turma();
-				TurmaDAO dDAO = new TurmaDAO();
-				
-				d.setCod(Integer.parseInt(txtCodigo.getText()));
-				d.setSerie(txtSerie.getText());
-				d.setPeriodo(txtPeriodo.getText());
-				
-				dDAO.alterar(d);
-				
-				JOptionPane.showMessageDialog(btnInserir, "Dados da turma alterado com sucesso");
-			}
-		});
-		btnAlterar.setBounds(316, 260, 287, 205);
-		frmTurma.getContentPane().add(btnAlterar);
-		
+		btnListar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnListar.setBounds(10, 453, 225, 217);
+		panel.add(btnListar);
+
 		JButton btnDeletar = new JButton("Deletar");
-		btnDeletar.setFont(new Font("Arial", Font.PLAIN, 24));
 		btnDeletar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				TurmaDAO dDAO = new TurmaDAO();
-				
+
 				dDAO.delete(Integer.parseInt(txtCodigo.getText()));
-				
-				JOptionPane.showMessageDialog(btnInserir, "Turma deletada com sucesso");
+
+				JOptionPane.showMessageDialog(btnIncluir, "Turma deletada com sucesso");
 			}
 		});
-		btnDeletar.setBounds(316, 476, 287, 194);
-		frmTurma.getContentPane().add(btnDeletar);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(613, 11, 641, 659);
-		frmTurma.getContentPane().add(scrollPane);
-		scrollPane.setViewportView(tbTurmas);
+		btnDeletar.setFont(new Font("Arial", Font.PLAIN, 26));
+		btnDeletar.setBounds(236, 453, 225, 217);
+		panel.add(btnDeletar);
 	}
+
 }
